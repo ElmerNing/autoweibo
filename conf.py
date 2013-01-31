2f348464d3915faad2eef0da789efc6e1566fd6f
ElmerNing <ffxhina@gmail.com>
2013-01-31 15:06:14 +0800

    abc
    Signed-off-by: ElmerNing <ffxhina@gmail.com>

--- conf.py
+++ conf.py
@@ -1,5 +1,4 @@
 import sys
-import rule
 import urllib2
 
 #sina
@@ -7,7 +6,7 @@ APP_KEY = '377217891' #youre app key
 APP_SECRET = '723905a68a35ca6e2f30016230337e8f' #youre app secret  
 CALLBACK_URL = 'http://6.helloelmer.sinaapp.com/callback'
 ACCOUNT = 'ffxhina@163.com'#your email address
-PASSWORD = sys.argv[1] #'******'     #your pw
+PASSWORD = sys.argv[1]#"longyang22" #'******'     #your pw
 
 #gplus
 GPLUS_ENABLE = True
@@ -15,12 +14,6 @@ GPLUS_ENABLE = True
 GPLUS_URLS = (
     "https://plus.google.com/communities/112204979951069292983",
 )
-
-GPLUS_RULES = {
-    "comments" : rule.bigger_than(4),
-    "forward" : rule.bigger_than(7),
-    "plus" : rule.bigger_than(10),
-}
 
 
 #proxy
